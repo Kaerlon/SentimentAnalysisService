d5b0b5df4e3bdbc3afcbfdfbcac4421e658f547a
Kaerlon <fargusus@gmail.com>
2021-09-11 20:52:16 +0300

    fix trainng

--- SentimentAnalysis.MlNet/Model/SentimentData.cs
+++ SentimentAnalysis.MlNet/Model/SentimentData.cs
@@ -4,7 +4,7 @@ namespace SentimentAnalysis.MlNet.Model
 {
     public class SentimentData
     {
-        [LoadColumn(0), ColumnName("Features")]
+        [LoadColumn(0)]
         public string Message { get; set; }
         [LoadColumn(1), ColumnName("Label")]
         public int Result { get; set; }

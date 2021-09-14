a342c5217a2077e8c9f43ef21f118dac756d477d
Kaerlon <fargusus@gmail.com>
2021-09-14 11:52:14 +0300

    Update

---
+++ SentimentAnalysis.Test/Models/ResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysis.Test.Models
+{
+	public class ResponseModel
+	{
+		public int Prediction { get; set; }
+		public Dictionary<LabelEnums, float> Scores { get; set; }
+	}
+	public enum LabelEnums : byte
+	{
+		Negative = 0,
+		Positive = 1,
+		Neutral = 2,
+	}
+}
